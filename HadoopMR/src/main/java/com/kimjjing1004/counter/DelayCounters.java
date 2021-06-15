5f12117c2c1daca6ceb642ba58a399526bdba417
kimjjing1004 <kimjjing1004@naver.com>
2021-06-15 17:48:34 +0900

    DelayCount Driver, Mapper, Reducer

---
+++ HadoopMR/src/main/java/com/kimjjing1004/counter/DelayCounters.java
@@ -0,0 +1,5 @@
+package com.kimjjing1004.counter;
+
+public enum DelayCounters {
+	not_available_arrival, scheduled_arrival, early_arrival, not_available_departure, scheduled_departure, early_departure;
+}
---
+++ HadoopMR/src/main/java/com/kimjjing1004/driver/DelayCount.java
@@ -0,0 +1,58 @@
+package com.kimjjing1004.driver;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import com.kimjjing1004.mapper.DelayCountMapper;
+import com.kimjjing1004.reducer.DelayCountReducer;
+
+public class DelayCount extends Configured implements Tool {
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new DelayCount(), args);
+		System.out.println("MR-Job Result: " + res);
+
+	}
+	
+
+	public int run(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		// GenericOptionParser에서 제공하는 파라미터를 제외한 나머지 파라미터 가져오기
+		String[] otherArgs = new GenericOptionsParser(getConf(), args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: DelayCount <in> <out>");
+			System.exit(2);
+		}
+		Job job = Job.getInstance(getConf(), "DelayCount");
+		
+		
+		job.setJarByClass(DelayCount.class);
+		job.setMapperClass(DelayCountMapper.class);
+		job.setReducerClass(DelayCountReducer.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		
+		job.waitForCompletion(true);
+		return 0;
+	}
+	
+
+}
