b5e2695a080ebe365c7d07aa773868743053a0c7
kimjjing1004 <kimjjing1004@naver.com>
2021-06-16 16:09:13 +0900

    DelayCountWithDateKey Driver, Mapper, Reducer

---
+++ HadoopMR/src/main/java/com/kimjjing1004/common/GroupKeyPartitioner.java
@@ -0,0 +1,18 @@
+package com.kimjjing1004.common;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class GroupKeyPartitioner extends Partitioner<DateKey, IntWritable> {
+
+	@Override
+	public int getPartition(DateKey key, IntWritable value, int numPartitions) {
+		// TODO Auto-generated method stub
+		int hash = key.getYear().hashCode();
+		int partition = hash % numPartitions;
+		return partition;
+	}
+	
+	
+
+}
